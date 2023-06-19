c5f835e4a7c69a8f1778d709f665b262c97ae361
Wamy-Dev <25761442+Wamy-Dev@users.noreply.github.com>
2023-06-18 22:11:51 -0500

    handling of page 41 bug

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,10 +1,17 @@
 import { connectPagination } from "react-instantsearch-dom";
 import Pagination from "@mui/material/Pagination";
 import PaginationItem from "@mui/material/PaginationItem";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import Box from "@mui/material/Box";
 
 const CustPagination = ({ currentRefinement, nbPages, refine, createURL, total }) => {
   var pagesToDisplay = Math.round(total / 5)
+  if (pagesToDisplay > 41) {
+    pagesToDisplay = 41
+  }
   return (
+    <>
     <Pagination
       page={currentRefinement}
       count={pagesToDisplay}
@@ -24,6 +31,19 @@ const CustPagination = ({ currentRefinement, nbPages, refine, createURL, total }
     >
       <PaginationItem />
     </Pagination>
+    {currentRefinement > 40 ? (
+      <Box sx={{ width: "100%", maxWidth: 450, paddingTop: "20px" }}>
+        <Alert
+          severity="error"
+          variant="outlined"
+          sx={{ borderRadius: "10px" }}
+        >
+          <AlertTitle>Rezi has more results, I promise</AlertTitle>
+            Rezi actually have over 250k results, but it is limited to 40 pages of results. Please refine your search to get more results.
+        </Alert>
+      </Box>
+    ) : null}
+    </>
   )
 }
   
