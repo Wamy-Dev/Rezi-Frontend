55897bb01e462f579e9d9bcb242f17aff5e19bf9
David <davidisadev@gmail.com>
2023-07-13 15:42:03 -0500

    added sa-events

--- src/components/searchbox.js
+++ src/components/searchbox.js
@@ -1,6 +1,6 @@
 import { connectSearchBox } from "react-instantsearch-dom";
 import TextField from "@mui/material/TextField";
-
+const sa_event = window.sa_event;
 const SearchBox = ({ currentRefinement, isSearchStalled, refine }) => (
   <form noValidate role="search" onSubmit={(event) => event.preventDefault()}>
     <TextField
@@ -12,6 +12,7 @@ const SearchBox = ({ currentRefinement, isSearchStalled, refine }) => (
       value={currentRefinement}
       onChange={(event) => {
         event.preventDefault();
+        sa_event("search")
         refine(event.currentTarget.value);
       }}
     />
