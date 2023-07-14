a9fd99b830629fa2fb0976732ca37593fee9709d
David <davidisadev@gmail.com>
2023-07-14 09:52:40 -0500

    Fixes #15

--- src/components/searchbox.js
+++ src/components/searchbox.js
@@ -1,6 +1,5 @@
 import { connectSearchBox } from "react-instantsearch-dom";
 import TextField from "@mui/material/TextField";
-const sa_event = window.sa_event;
 const SearchBox = ({ currentRefinement, isSearchStalled, refine }) => (
   <form noValidate role="search" onSubmit={(event) => event.preventDefault()}>
     <TextField
@@ -12,7 +11,6 @@ const SearchBox = ({ currentRefinement, isSearchStalled, refine }) => (
       value={currentRefinement}
       onChange={(event) => {
         event.preventDefault();
-        sa_event("search")
         refine(event.currentTarget.value);
       }}
     />
